0fe2ea2283fb4a5415801e11d55358760b9a34de
MAYANK2454-19 <mk2599599@gmail.com>
2026-06-23 20:28:23 +0530

    new code

---
+++ cpp/basicio.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std ;
+int main (){
+//variables
+int age ;
+float height;
+char initial ;
+string name ;
+//inputs
+cout << "enter your age " << endl ;
+cin >> age ;
+cout << "enter your height in metres" << endl ;
+cin >> height ;
+cout << "enter your initial" << endl ;
+cin >> initial ;
+cout << "enter your name " << endl ;
+cin >> name ;
+//output
+cout << "your name is " << name << " your age is " << age << " your initial is " << initial << " your height is " << height << " m " << endl ;
+
+return 0;
+
+}
